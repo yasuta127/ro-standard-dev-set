0cd4b1a0534faf040ee509ba846919b54149b83a
yasuta127 <yasudadev1020@gmail.com>
2022-04-04 12:59:00 +0900

    Add UnityObjectUtilTest

---
+++ Packages/RoStandardDevSet/Tests/FooMonoBehaviour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace RoStandardDevSet.Tests
+{
+    public class FooMonoBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+    }
+}
---
+++ Packages/RoStandardDevSet/Tests/UnityObjectUtilTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+
+namespace RoStandardDevSet.Tests
+{
+    public class UnityObjectUtilTest
+    {
+        [UnityTest]
+        public IEnumerator UtilTest()
+        {
+            // Start前
+            var go = new GameObject("TestObject");
+
+            yield return null;
+            // Start後
+
+            FooMonoBehaviour fooMonoBehaviour = go.GetOrAddComponent<FooMonoBehaviour>();
+            Assert.IsNotNull(fooMonoBehaviour);
+            BarMonoBehaviour barMonoBehaviour = fooMonoBehaviour.GetOrAddComponent<BarMonoBehaviour>();
+            Assert.IsNotNull(barMonoBehaviour);
+            FooMonoBehaviour fooMonoBehaviour2 = go.GetOrAddComponent<FooMonoBehaviour>();
+            FooMonoBehaviour fooMonoBehaviour3 = fooMonoBehaviour.GetOrAddComponent<FooMonoBehaviour>();
+            Assert.AreEqual(fooMonoBehaviour, fooMonoBehaviour2);
+            Assert.AreEqual(fooMonoBehaviour, fooMonoBehaviour3);
+
+            var newInstanceFooMonoBehaviour = fooMonoBehaviour.InstantiateOf();
+            Assert.IsNotNull(newInstanceFooMonoBehaviour);
+            Assert.AreNotEqual(fooMonoBehaviour, newInstanceFooMonoBehaviour);
+        }
+    }
+}
