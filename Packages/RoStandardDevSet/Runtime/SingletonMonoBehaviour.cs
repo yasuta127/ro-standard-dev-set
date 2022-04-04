a0fccab472c0b468e9d3bb9a1a77f4419bbf6387
yasuta127 <yasudadev1020@gmail.com>
2022-04-04 13:25:35 +0900

    Add SingletonMonoBehaviour and test

---
+++ Packages/RoStandardDevSet/Runtime/SingletonMonoBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace RoStandardDevSet
+{
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Type t = typeof(T);
+                    instance = (T)FindObjectOfType(t);
+                    if (instance == null)
+                    {
+                        var go = new GameObject(t.Name);
+                        instance = go.AddComponent<T>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            CheckInstance();
+        }
+
+        /// <summary>
+        /// 他のGameObjectにアタッチされているか調べる。
+        /// アタッチされている場合は自身を破棄する。
+        /// </summary>
+        /// <returns></returns>
+        protected bool CheckInstance()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                return true;
+            }
+
+            if (Instance == this)
+            {
+                return true;
+            }
+
+            Destroy(this);
+            return false;
+        }
+    }
+}
