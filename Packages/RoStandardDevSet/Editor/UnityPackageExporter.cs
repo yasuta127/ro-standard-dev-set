ff5654e794add4ac76171972f2e9e5af0bca9f93
yasuta127 <yasudadev1020@gmail.com>
2022-04-04 12:10:15 +0900

    Add UnityPackageExporter

---
+++ Packages/RoStandardDevSet/Editor/UnityPackageExporter.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using UnityEditor;
+
+namespace RoStandardDevSet.Editor
+{
+    public static class UnityPackageExporter
+    {
+        private const string packageName = "RoStandardDevSet";
+        private const string packagePath = "RoStandardDevSet";
+        private const string exportPath = "Builds";
+
+        public static void Export()
+        {
+            var exportName = $"{exportPath}/{packageName}.unitypackage";
+            var directory = new FileInfo(exportName).Directory;
+            if (directory is { Exists: false })
+            {
+                directory.Create();
+            }
+
+            AssetDatabase.ExportPackage($"Packages/{packagePath}", exportName, ExportPackageOptions.Recurse);
+        }
+    }
+}
